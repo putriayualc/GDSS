6d03fc28d55e508508fcb3bfe817c4de1cf9921d
Putri A. Aliciawati <aliciaputri706@gmail.com>
2024-05-09 22:05:21 +0700

    BORDA

--- src/Borda.java
+++ src/Borda.java
@@ -1,3 +1,81 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
 public class Borda {
+    private ArrayList<DecisionMaker> decisionMakers;
+    private ArrayList<Alternatif> alternatifs;
+    private int[][] peringkat;
+
+    public Borda(ArrayList<DecisionMaker> dm, ArrayList<Alternatif> at) {
+        this.decisionMakers = dm;
+        this.alternatifs = at;
+        //peringkat hasil perhitangan DSS MABAC
+        peringkat = new int[][]{
+            {3,4,4},
+            {4,2,2},
+            {8,7,8},
+            {5,8,7},
+            {7,5,6},
+            {2,1,3},
+            {1,3,1},
+            {6,6,5}
+        };
+    }
+
+    public void hitung(){
+        int[][] totalPeringkat = totalPeringkat();
+        int[] pembobot = bobot();
+        int[] skorBORDA = skorBorda(pembobot, totalPeringkat);
+        System.out.println("+====HASIL AKHIR BORDA====+");
+        System.out.printf("| %-10s | %-10s |\n", "Alternatif", "Skor BORDA");
+        System.out.println("===========================");
+        for (int i = 0; i < alternatifs.size(); i++) {
+            System.out.printf("| %-10s | %-10s |\n",alternatifs.get(i).getNamaAlternatif(), skorBORDA[i]);
+        }
+        System.out.println("===========================");
+
+        int rank1=0;
+        int max = 0;
+        for (int i = 0; i < skorBORDA.length; i++) {
+            if (skorBORDA[i] > max) {
+                max = skorBORDA[i];
+                rank1 = i;
+            }
+        }
+
+        System.out.println("Menurut perhitungan dengan metode BORDA, Alternatif " + alternatifs.get(rank1).getNamaAlternatif() +" merupakan alternatif terbaik dengan skor akhir BORDA = "+skorBORDA[rank1]);
+    }
+
+    private int[][] totalPeringkat(){
+        int[][] totPer = new int[peringkat.length][peringkat.length];
+        for (int i = 0; i < peringkat.length; i++) {
+            for (int j = 0; j < peringkat[0].length; j++) {
+                int rank = peringkat[i][j];
+                totPer[i][rank-1]++;
+            }
+        }
+        return totPer;
+    }
+
+    private int[] bobot(){
+        int[] bobot = new int[alternatifs.size()];
+        for (int i = 0; i < bobot.length; i++) {
+            bobot[i]=bobot.length-(i+1);
+        }
+        return bobot;
+    }
+
+    private int[] skorBorda(int[] bobot, int[][] peringkat){
+        int[] skor = new int[alternatifs.size()];
+        for (int i = 0; i < peringkat.length; i++) {
+            for (int j = 0; j < peringkat[0].length; j++) {
+                skor[i] += peringkat[i][j]*bobot[j]; 
+            }
+        }
+        return skor;
+    }
     
+
 }
--- src/GDSSMain.java
+++ src/GDSSMain.java
@@ -1,5 +1,45 @@
+import java.util.ArrayList;
+
 public class GDSSMain {
     public static void main(String[] args) {
         System.out.println("GDSS");
+        ArrayList<Kriteria> kriteriaGita = new ArrayList<>();
+        kriteriaGita.add(new Kriteria("Harga", 15, false));
+        kriteriaGita.add(new Kriteria("Jarak dengan Kampus", 10, false));
+        kriteriaGita.add(new Kriteria("Fasilitas kos", 20, true));
+        kriteriaGita.add(new Kriteria("Luas Kamar", 25, true));
+        kriteriaGita.add(new Kriteria("Kebersihan", 15, true));
+        kriteriaGita.add(new Kriteria("Keamanan", 15, true));
+
+        ArrayList<Kriteria> kriteriaAyah = new ArrayList<>();
+        kriteriaAyah.add(new Kriteria("Harga", 20, false));
+        kriteriaAyah.add(new Kriteria("Jarak dengan Kampus", 15, false));
+        kriteriaAyah.add(new Kriteria("Fasilitas kos", 15, true));
+        kriteriaAyah.add(new Kriteria("Luas Kamar", 10, true));
+        kriteriaAyah.add(new Kriteria("Kebersihan", 15, true));
+        kriteriaAyah.add(new Kriteria("Keamanan", 25, true));
+
+        ArrayList<Kriteria> kriteriaIbu = new ArrayList<>();
+        kriteriaIbu.add(new Kriteria("Harga", 25, false));
+        kriteriaIbu.add(new Kriteria("Jarak dengan Kampus", 10, false));
+        kriteriaIbu.add(new Kriteria("Fasilitas kos", 15, true));
+        kriteriaIbu.add(new Kriteria("Luas Kamar", 10, true));
+        kriteriaIbu.add(new Kriteria("Kebersihan", 20, true));
+        kriteriaIbu.add(new Kriteria("Keamanan", 20, true));
+
+        ArrayList<Alternatif> alternatifs = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            alternatifs.add(new Alternatif("A"+(i+1)));   
+        }
+
+        ArrayList<DecisionMaker> dm = new ArrayList<>();
+        dm.add(new DecisionMaker("Gita", kriteriaGita));
+        dm.add(new DecisionMaker("Ayah", kriteriaAyah));
+        dm.add(new DecisionMaker("Ibu", kriteriaIbu));
+
+        Borda bd = new Borda(dm, alternatifs);
+
+        bd.hitung();
+
     }
 }
